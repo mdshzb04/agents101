7c48fd92b2846e0a832b1fd9ebcdbcde483d5dda
mohammed shazeb <mohammedshazeb10@gmail.com>
2025-12-09 23:28:24 +0530

    Refactor runLLM function to simplify parameters

--- src/llm.ts
+++ src/llm.ts
@@ -1,22 +1,12 @@
 import type { AIMessage } from '../types' 
 import { openai } from './ai'
 
-// Define an async function runLLM that sends messages to the LLM and return assistant Reply
-export const runLLM = async ({
-  model = 'gpt-4o-mini',
-  messages,
-  temperature = 0.1,
-}: {
-  messages: AIMessage[]
-  temperature?: number
-  model?: string
-}) => {
-  // request
+export const runLLM = async ({messages}:{messages:AIMessage[]})=>{
+
   const response = await openai.chat.completions.create({
     model:'gpt-4o-mini',
+    temperature :0.1,
     messages,
-    temperature :2,
   })
-  // assistant message 
   return response.choices[0].message
 }
